15927c14411b80ead4ebb7d72d535e8604e7e18e
OrazbaevaMariam <mariyam@MacBook-Air-Mariyam.local>
2025-06-13 13:58:59 +0300

    modules

--- src/app/views/main-pages/main-pages-routing.module.ts
+++ src/app/views/main-pages/main-pages-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {MainComponent} from "./main/main.component";
 import {CatalogComponent} from "./catalog/catalog.component";
+import {ProductComponent} from "../products/product/product.component";
 
 const routes: Routes = [
   {path: '', component: MainComponent},
   {path: 'catalog', component: CatalogComponent},
+  {path: 'catalog/product/:id', component: ProductComponent},
 ];
 
 @NgModule({
